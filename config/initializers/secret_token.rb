fe371e598fbfd633eb9711f042771277411f4053
unknown <Venus@.(none)>
2013-06-23 20:03:12 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstChallenge::Application.config.secret_token = 'ceeff43b341d958be98f4b70f0fc3a5f8096eaf3ba16ff751397eacc6e29d881e7c3229ab9336700eee761a8fac375501fe7b57e68f24d9f8ec058023ce0e5ca'
